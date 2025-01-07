2b18c30820fe32aafdadc18dcc0211ae752126b2
MaggieJiang7 <147207333+MaggieJiang7@users.noreply.github.com>
2025-01-07 13:21:12 -0500

    Added basic jumping physics

--- Avatar.java
+++ Avatar.java
@@ -9,8 +9,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Avatar extends Actor
 {
-    SimpleTimer animationTimer = new SimpleTimer();
     GreenfootImage sussyImage= new GreenfootImage("images/sussy.png");
+    private SimpleTimer timer = new SimpleTimer();
+    
+    private int velocity;
+    private int gravity=1;
+    private int counter = 0;
     /**
      * Act - do whatever the Avatar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the//Contructor environment.
@@ -19,33 +23,53 @@ public class Avatar extends Actor
     public Avatar(){
         setImage(sussyImage);
         sussyImage.scale(75,80);
+        velocity = 0;
     }
     
+    public void checkWarp(){
+        //Check if the avatar goes out of the right edge (X > world width)
+        if (getX()<1) {
+            setLocation(getWorld().getWidth()-2, getY()); //Warp to the right side
+        }
+        // Check if the avatar goes out of the left edge (X > world width)
+        if (getX()>getWorld().getWidth()-2) {
+            setLocation(0, getY());  // Wrap to the right side
+        }
+    }
     
-    public void act()
-    {
+    public void checkKeys(){
+        fall(); // Applies gravity to character
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-10);
+            move(-5);
         }
-        else if(Greenfoot.isKeyDown("right"))
+        if(Greenfoot.isKeyDown("right"))
         {
-            move(10);
+            move(5);
         }
-        System.out.println(getX());
-        if (getX()<1) {
-            setLocation(getWorld().getWidth()-2, getY());  // Wrap to the right side
-        }
-        
-        // Check if the avatar goes out of the right edge (X > world width)
-        if (getX()>getWorld().getWidth()-2) {
-            setLocation(0, getY());  // Wrap to the left side
-        }
-        
         if(Greenfoot.isKeyDown("up")){
-            int y=getY() - 5;
-            setLocation(getX(), y);
-            ,,,
+           jump();
         }
+    }
+    
+    public void jump(){
+        if(getY() > getWorld().getHeight() - 50){
+            velocity= - 20;
+        }
+    } 
+    
+    public void fall(){
+        setLocation(getX(), getY() + velocity); // Updates location
+        if(getY()> getWorld().getHeight() - 50){
+            velocity = 0; // Makes sure character does not fall beyond surface
+        } else {
+            velocity += gravity; // Makes character fall with gravity if not on surface
+        }
+    }
+    public void act()
+    {
+        fall();
+        checkWarp();
+        checkKeys();
     }
 }
