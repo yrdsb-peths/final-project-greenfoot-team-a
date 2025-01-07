4a7bf6ddac086901a482fa966acc82e0ff989893
Carlisa100 <348823873@gapps.yrdsb.ca>
2025-01-07 13:04:58 -0500

    Menu Screen Code

---
+++ MenuScreen.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MenuScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MenuScreen extends World
+{
+
+    /**
+     * Constructor for objects of class MenuScreen.
+     * 
+     */
+    public MenuScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+       
+         
+        setBackground(new GreenfootImage("FinalISPBackground.png"));
+        addObject(new Label ("Welcome to the Type to Fight Game!"),300, 200); 
+        addObject(new Label ("Click the Start Button to read the instructions and begin the game!"),300, 220);
+        addObject(new Button(this::startInstructions), 300, 300); 
+        
+        String startTwo = "Start";
+        addObject(new Label ("Start"), 300, 270);
+    }
+
+    private void startInstructions() 
+    {
+        Greenfoot.setWorld(new InstructionScreen());
+    }
+}
+
