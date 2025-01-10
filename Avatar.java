0a76439c63d64b39df1130986cbee2772bfb890f
MaggieJiang7 <147207333+MaggieJiang7@users.noreply.github.com>
2025-01-10 13:27:23 -0500

    Animate Avatar Running
    Adding running animation

--- Avatar.java
+++ Avatar.java
@@ -12,24 +12,89 @@ public class Avatar extends Actor {
 
     private int velocity = 0; // The character's vertical velocity
     private int gravity = 1; // Gravity pulling the character down
-    private int maxJumpVelocity = -20; // Maximum upward velocity for a fully charged jump
-    private int minJumpVelocity = -10; // Initial jump velocity for a short jump
+    private int maxJumpVelocity = -15; // Maximum upward velocity for a fully charged jump
+    private int minJumpVelocity = -5; // Initial jump velocity for a short jump
     private int jumpCharge = 0; // Tracks how long the jump key is held
-    private int maxChargeTime = 15; // Limit for charging the jump
     private boolean isJumping = false; // Tracks if the character is in the air
     private boolean keyReleased = true; // Tracks if the jump key has been released
 
-    private int groundLevel; // Ground level based on the world's height
+    // Image variables
+    GreenfootImage[] idleRight = new GreenfootImage[6];
+    GreenfootImage[] idleLeft = new GreenfootImage[6];
+    GreenfootImage[] runRight = new GreenfootImage[6];
+    GreenfootImage[] runLeft = new GreenfootImage[6];
+    // Variables to handle direction cat is facing
+    String facing = "right";
+    Boolean isMoving = false;
+    SimpleTimer animationTimer = new SimpleTimer();
 
     public Avatar() {
-        setImage(sussyImage);
-        sussyImage.scale(75, 80);
-        groundLevel = getWorld() == null ? 0 : getWorld().getHeight() - 100;
+    
+        // Load idleRight images (starting from 0 to 5)
+        for(int i = 0; i < 6; i++) {  // Fixed the starting index here
+            idleRight[i] = new GreenfootImage("images/sprites/avatar/idle/idle" + (i + 1) + ".png");
+            idleRight[i].scale(100, 100);
+        }
+    
+        // Load idleLeft images (mirroring them horizontally)
+        for(int i = 0; i < 6; i++) {  
+            idleLeft[i] = new GreenfootImage("images/sprites/avatar/idle/idle" + (i + 1) + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100); 
+        }
+    
+        // Load runRight images
+        for(int i = 0; i < 6; i++) {
+            runRight[i] = new GreenfootImage("images/sprites/avatar/run/run" + (i + 1) + ".png");
+            runRight[i].scale(100, 100);
+        }
+    
+        // Load runLeft images (mirroring them horizontally)
+        for(int i = 0; i < 6; i++) {
+            runLeft[i] = new GreenfootImage("images/sprites/avatar/run/run" + (i + 1) + ".png");
+            runLeft[i].mirrorHorizontally();
+            runLeft[i].scale(100, 100); 
+        }
+    
+        // Set the initial image to idleRight[0]
+        setImage(idleRight[0]);
+        animationTimer.mark();
     }
 
-    @Override
-    protected void addedToWorld(World world) {
-        groundLevel = world.getHeight() - 100; // Initialize ground level after the actor is added
+    int imageIndex = 0;
+    public void animateAvatar()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(isMoving == false)
+        {
+            if(facing.equals("right"))
+            {
+                setImage(idleRight[imageIndex]);
+                imageIndex = (imageIndex + 1) % idleRight.length;
+            }
+            else
+            {
+                setImage(idleLeft[imageIndex]);
+                imageIndex = (imageIndex + 1) % idleLeft.length;
+            }
+        }
+        else
+        {
+            if(facing.equals("right"))
+            {
+                setImage(runRight[imageIndex]);
+                imageIndex = (imageIndex + 1) % runRight.length;
+            }
+            else
+            {
+                setImage(runLeft[imageIndex]);
+                imageIndex = (imageIndex + 1) % runLeft.length;
+            }
+        }
     }
 
     public void checkWarp() {
@@ -44,16 +109,30 @@ public class Avatar extends Actor {
     public void checkKeys() {
         if (Greenfoot.isKeyDown("left")) {
             move(-5);
-        }
-        if (Greenfoot.isKeyDown("right")) {
+            facing = "left";
+            if(isMoving == false) {
+                isMoving = true;
+                imageIndex = 0;
+            }
+        } else if (Greenfoot.isKeyDown("right")) {
             move(5);
+            facing = "right";
+            if(isMoving == false) {
+                isMoving = true;
+                imageIndex = 0;
+            }
+        } else {
+            if(isMoving == true)
+            {
+                isMoving = false;
+                imageIndex = 0;
+            }
         }
     }
 
     public void checkJump() {
         boolean jumpKeyHeld = Greenfoot.isKeyDown("up");
 
-        // Handle jump initiation
         if (jumpKeyHeld && keyReleased) {
             if (!isJumping) {
                 velocity = minJumpVelocity; // Start the jump
@@ -65,7 +144,8 @@ public class Avatar extends Actor {
 
         // Handle jump charging while key is held
         if (jumpKeyHeld && isJumping) {
-            if (jumpCharge < maxChargeTime) {
+            // Tracks how long button was held (max 15 game cycles)
+            if (jumpCharge < 15) {
                 jumpCharge++;
                 velocity--; // Increase jump strength
                 if (velocity < maxJumpVelocity) {
@@ -84,8 +164,8 @@ public class Avatar extends Actor {
         setLocation(getX(), getY() + velocity); // Updates location
 
         // If the character hits or sinks below the ground, reset
-        if (getY() >= groundLevel) {
-            setLocation(getX(), groundLevel); // Snap to ground level
+        if (getY() >= getWorld().getHeight() - 100) {
+            setLocation(getX(), getWorld().getHeight() - 100); // Snap to ground level
             velocity = 0; // Stop falling
             isJumping = false; // Allow jumping again
         } else {
@@ -95,8 +175,10 @@ public class Avatar extends Actor {
 
     public void act() {
         fall();
+        animateAvatar();
         checkJump();
         checkWarp();
         checkKeys();
+        System.out.println(isMoving);
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -3,9 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World 
 {    
     public MyWorld() {
+        super(400, 600, 1);
         Avatar avatar = new Avatar();
         addObject(avatar, 200, 300);
 
-        super(400, 600, 1);
     }
 }
