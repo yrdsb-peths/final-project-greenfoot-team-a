0cc25b9141e20d0b5c3532f4d7b3aea1b50499bf
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2025-01-14 13:06:17 -0500

    Reset ShopWorld

---
+++ ShopWorld.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class ShopWorld extends World
+{
+    public ShopWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
